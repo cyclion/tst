d31f7eb49ad36e9db137f0970f4004cc9a7ba830
Vitalii Dasaev <vitalyd87@gmail.com>
2018-05-12 07:19:48 +0300

    Update env-icon.js - add version

--- tm/env-icon.js
+++ tm/env-icon.js
@@ -1,6 +1,7 @@
 // ==UserScript==
 // @name         Title add T (test), D (dev), L (localhost)
 // @namespace    http://tampermonkey.net/
+// @version      1.0.0
 // @description  try to take over the world!
 // @author       You
 // @match        https://*.lykkex.net/*
